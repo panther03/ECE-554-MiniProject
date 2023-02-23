20c9c49e4c9e9bf2441cb03791d1a3901ffd20aa
Julien de Castelnau <tripod320@gmail.com>
2023-02-23 10:49:13 -0600

    Comment testbench thoroughly

--- tb/pkg/spart_tb_tasks.sv
+++ tb/pkg/spart_tb_tasks.sv
@@ -1,8 +1,8 @@
 `timescale 1ns/100ps
 // WHOLE MODULE ASSUMES A 50MHZ CLOCK!!
 package spart_tb_tasks;
-
-    localparam RX_WAIT = 1_000_000; // 1 million cycles as a large upper limit
+    // Uncomment this if you want UART debugging messages
+    //`define UART_DEBUGGING
 
     // This function takes in an integer corresponding to a UART baud rate
     // and spits out the value that you would load into the databuffer register for that baud rate
@@ -16,14 +16,13 @@ package spart_tb_tasks;
     task automatic send_uart_tx(ref clk, ref TX, input int baud_int, input [7:0] tx_data);
         int baud_wait = calculate_baud(baud_int) + 1;
         bit [9:0] tx_data_temp = {1'b1, tx_data, 1'b0}; // start and stop
-        $display("send_uart_tx called @ time=%t", $time());
-        // Just to wait for the start bit
+        `ifdef UART_DEBUGGING $display("send_uart_tx called @ time=%t", $time()); `endif
         for (int i = 0; i <= 9; i++) begin
             if (tx_data_temp[i]) begin
-                $display("Setting TX = 1 @ %t", $time());
+                `ifdef UART_DEBUGGING $display("Setting TX = 1 @ %t", $time()); `endif
                 TX = 1;
             end else begin
-                $display("Setting TX = 0 @ %t", $time());
+                `ifdef UART_DEBUGGING $display("Setting TX = 0 @ %t", $time()); `endif
                 TX = 0;
             end
             repeat (baud_wait) @(posedge clk);
@@ -36,13 +35,13 @@ package spart_tb_tasks;
         bit [8:0] rx_data_temp = 9'h0;
         int baud_wait = calculate_baud(baud_int) + 1;
         int baud_wait_half = (baud_wait >> 1);
-        $display("baud (full/half): %d/%d", baud_wait, baud_wait_half);
+        `ifdef UART_DEBUGGING $display("baud (full/half): %d/%d", baud_wait, baud_wait_half); `endif
 
-        $display("rx start %t", $time);
+        `ifdef UART_DEBUGGING $display("rx start %t", $time); `endif
         // Advance to halfway through the transaction
         // That way we are reading the remaining bits halfway through (safer)
         repeat (baud_wait_half) @(posedge clk);
-        $display("advance to half %t", $time);
+        `ifdef UART_DEBUGGING $display("advance to half %t", $time); `endif
         assert(RX == 0); // start bit should stay 0, this is an early sanity check
 
         // A UART transaction has started
@@ -51,32 +50,19 @@ package spart_tb_tasks;
             repeat (baud_wait) @(posedge clk);
             if (RX) begin
                 rx_data_temp[i] = 1'b1;
-                $display("new bit 1 %t", $time);
+                `ifdef UART_DEBUGGING $display("new bit 1 %t", $time); `endif
             end else begin
                 rx_data_temp[i] = 1'b0;
-                $display("new bit 0 %t", $time);
+                `ifdef UART_DEBUGGING $display("new bit 0 %t", $time); `endif
             end
         end
 
         // Wait for the transaction to completely end
         repeat (baud_wait_half + 1) @(posedge clk);
-        $display("ending byte %t", $time);
+        `ifdef UART_DEBUGGING $display("ending byte %t", $time); `endif
 
         // Ignore stop
         rx_data = rx_data_temp[7:0];
-    endtask //automatic
-
-    task automatic assert_msg(input cond, input string msg);
-        if (cond) begin
-            $display("Condition passed: %s @ time = %t", msg, $time);
-        end else begin
-            $display("Condition failed: %s @ time = %t", msg, $time);
-            $stop();
-        end
     endtask
 
-    //task automatic spart_reg_read(ref clk, ref logic [7:0] db, ref db_we, ref iocs_n, ref iorw_n, ref logic [1:0] ioaddr );
-    //task automatic spart_reg_read(ref clk, ref spart_reg_bus b);
-        
-    //endtask //automatic
 endpackage
--- tb/spart/spart_tb.sv
+++ tb/spart/spart_tb.sv
@@ -2,6 +2,7 @@
 module spart_tb ();
 
 import MiniLab_defs::*;
+import generic_tasks::*;
 import spart_tb_tasks::*;
 
 localparam QUEUE_SIZE = 8;
@@ -14,8 +15,12 @@ interface spart_reg_bus (input clk);
     spart_ioaddr_t ioaddr;
     logic [7:0] databus_out;
 
+    // Handle the tristate to the SPART module
+    // We want to be driving whenever the SPART isn't
     wire [7:0] databus = (iocs_n || !iorw_n) ? databus_out : 8'hZ;
 
+    // Both of these read and write tasks take exclusive contrtol over the bus
+    // The testbench must take safety precautions to prevent collisions between these in fork/join blocks
     task automatic spart_reg_write (input spart_ioaddr_t addr, input logic [7:0] wdata);
         iocs_n = 0;
         iorw_n = 0;
@@ -67,9 +72,13 @@ reg [7:0] srb_reg_temp;
 // store the whole databuffer so we can also have high & low
 reg [15:0] db_temp;
 
+// The always block down below which acts as our automatic UART module puts
+// its data in this variable and uses the new_.. as a ready flag
 reg [7:0] uart_rx_temp;
 reg new_uart_rx_data;
 
+// Store when the FIFOs are half full. 
+// We use these so we can test the varying read/write rates (fill queue back up when at half)
 reg tx_fifo_half_full;
 reg rx_fifo_half_full;
 
@@ -79,14 +88,14 @@ reg [7:0] rx_fifo_stim [STIM_QUEUE_SIZE-1:0];
 
 // we use these when filling or emptying the fifos
 integer tx_fill_fifo_ind;
-integer rx_empty_fifo_ind; 
+integer rx_empty_fifo_ind;
 
 // we increment these over the course of the uart reads and writes
 integer tx_fifo_ind;
 integer rx_fifo_ind;
 
 // the current baud rate
-integer    baud_temp;
+integer curr_baud;
 
 initial begin;
     // Initialize signals
@@ -99,19 +108,19 @@ initial begin;
     srb.iorw_n = 1;
     srb.ioaddr = ADDR_DBUF;
 
-    baud_temp = 0;
-
-    tx_fifo_ind = 0;
-    rx_fifo_ind = 0;
-
-    tx_fill_fifo_ind = 0;
-    rx_empty_fifo_ind = 0;
+    uart_rx_temp = 0;
+    new_uart_rx_data = 0;
 
     tx_fifo_half_full = 0;
     rx_fifo_half_full = 0;
 
-    uart_rx_temp = 0;
-    new_uart_rx_data = 0;
+    tx_fill_fifo_ind = 0;
+    rx_empty_fifo_ind = 0;
+
+    tx_fifo_ind = 0;
+    rx_fifo_ind = 0;
+
+    curr_baud = 0;
 
     // Initialize our stimulation data
     for (tx_fill_fifo_ind = 0; tx_fill_fifo_ind < TX_STIM_QUEUE_SIZE; tx_fill_fifo_ind++) begin
@@ -145,16 +154,16 @@ initial begin;
     // Test 3: Verify we can write to DB register //
     ///////////////////////////////////////////////
     // Set baud rate to 19200
-    baud_temp = 19200;
-    db_temp = calculate_baud(baud_temp);
+    curr_baud = 19200;
+    db_temp = calculate_baud(curr_baud);
     srb.spart_reg_write(ADDR_DBH, db_temp[15:8]);
     srb.spart_reg_write(ADDR_DBL, db_temp[7:0]);
-    db_temp = 0;
+    db_temp = 0; // reset so test is meaningful
 
-    // Check new baud rate
+    // Check that the baud rate register updated
     srb.spart_reg_read(ADDR_DBH, db_temp[15:8]);
     srb.spart_reg_read(ADDR_DBL, db_temp[7:0]);
-    assert_msg(calculate_baud(baud_temp) == db_temp[12:0], "Baud rate set to 19200");
+    assert_msg(calculate_baud(curr_baud) == db_temp[12:0], "Baud rate set to 19200");
 
     /////////////////////////////////////////////
     // Test 4: Basic read/write at 19200 baud //
@@ -167,35 +176,41 @@ initial begin;
             assert_msg(uart_rx_temp == 8'hAA, "Transmitted TX data matches queue data");
         end
         begin
-            send_uart_tx(clk, spart_rx, baud_temp, 8'h55);
+            send_uart_tx(clk, spart_rx, curr_baud, 8'h55);
             srb.spart_reg_read(ADDR_DBUF, srb_reg_temp);
             assert_msg(srb_reg_temp == 8'h55, "Received RX data matches queue data");
         end
     join    
 
     ///////////////////////////////////////
-    // Test 4: Interleaved reads/writes //
+    // Test 5: Interleaved reads/writes //
     /////////////////////////////////////
     // First set a new baud rate
-    baud_temp = 921600;
-    db_temp = calculate_baud(baud_temp);
+    curr_baud = 921600;
+    db_temp = calculate_baud(curr_baud);
     srb.spart_reg_write(ADDR_DBH, db_temp[15:8]);
     srb.spart_reg_write(ADDR_DBL, db_temp[7:0]);
 
     fork
         begin: FILL_TX_FIFO
+            // fill the TX fifo to full initially
             for (tx_fill_fifo_ind = 0; tx_fill_fifo_ind <= QUEUE_SIZE; tx_fill_fifo_ind++) begin
                 srb.spart_reg_write(ADDR_DBUF, tx_fifo_stim[tx_fill_fifo_ind]);
                 srb.spart_reg_read(ADDR_SREG, srb_reg_temp);
                 // conditional is there because once we've written the first queue entry, old pointer advances
                 // this is to be expected, as soon as TX consumes the data we should increment the old 
-                // point because the data has been consumed (TODO: check with eric?)
+                // point because the data has been consumed
                 assert_msg(srb_reg_temp[7:4] == QUEUE_SIZE-tx_fill_fifo_ind+((tx_fill_fifo_ind > 0) ? 0 : -1), "Status register (TX) increases with filling queue");
             end 
             assert_msg(tx_q_full, "Full signal asserted when queue is filled");
 
             wait(tx_fifo_half_full);
+            // we wait one posedge here so that the TX has a chance to read the next entry and decrement the old pointer
+            // simplifies our math so that this doesnt happen in the middle of when we're writing to the queue (like above)
+            @(posedge clk);
 
+            // once it's half full, we're going to fill it back to full
+            // this tests that the pointers are still working after having wrapped around fully
             for (tx_fill_fifo_ind = QUEUE_SIZE+1; tx_fill_fifo_ind < TX_STIM_QUEUE_SIZE; tx_fill_fifo_ind++) begin
                 srb.spart_reg_write(ADDR_DBUF, tx_fifo_stim[tx_fill_fifo_ind]);
                 srb.spart_reg_read(ADDR_SREG, srb_reg_temp);
@@ -207,8 +222,7 @@ initial begin;
             for (tx_fifo_ind = 0; tx_fifo_ind < TX_STIM_QUEUE_SIZE; tx_fifo_ind++) begin
                 wait(new_uart_rx_data);
                 new_uart_rx_data = 0;
-                assert_msg(uart_rx_temp == tx_fifo_stim[tx_fifo_ind], "Transmitted TX data matches queue data");
-                @(posedge clk);
+                assert_msg(uart_rx_temp == tx_fifo_stim[tx_fifo_ind], "Transmitted data from SPART matches queue data");
                 srb.spart_reg_read(ADDR_SREG, srb_reg_temp);
                 
                 if (tx_fifo_ind <= QUEUE_SIZE >> 1) begin
@@ -226,13 +240,13 @@ initial begin;
         begin: WRITE_RX_TO_SPART
             repeat (30) @(posedge clk); // delay a little to avoid reg reads colliding
             for (rx_fifo_ind = 0; rx_fifo_ind < STIM_QUEUE_SIZE; rx_fifo_ind++) begin
-                send_uart_tx(clk, spart_rx, 921600, rx_fifo_stim[rx_fifo_ind]);
+                send_uart_tx(clk, spart_rx, curr_baud, rx_fifo_stim[rx_fifo_ind]);
                 srb.spart_reg_read(ADDR_SREG, srb_reg_temp);
                 if (rx_fifo_ind < QUEUE_SIZE >> 1) begin
                     assert_msg(srb_reg_temp[3:0] == rx_fifo_ind+1, "Status register (RX) tracks after sending UART data");
                 end else begin
                     // math is because we emptied it
-                    assert_msg(srb_reg_temp[3:0] == (rx_fifo_ind+1 - (QUEUE_SIZE >> 1)), "Status register (TX) tracks after transmitting entry from re-filled queue");
+                    assert_msg(srb_reg_temp[3:0] == (rx_fifo_ind+1 - (QUEUE_SIZE >> 1)), "Status register (RX) tracks after transmitting entry from re-filled queue");
                 end
                 assert_msg(!rx_q_empty, "Empty signal is not high while data in RX queue");
                 if (rx_fifo_ind+1 == QUEUE_SIZE >> 1)
@@ -244,9 +258,9 @@ initial begin;
             // half full signal has been triggered. we're going to empty what's in there and let it be refilled.
             for (rx_empty_fifo_ind = 0; rx_empty_fifo_ind < (QUEUE_SIZE >> 1); rx_empty_fifo_ind++) begin
                 srb.spart_reg_read(ADDR_DBUF, srb_reg_temp);
-                assert_msg(srb_reg_temp == rx_fifo_stim[rx_empty_fifo_ind], "Received RX data matches queue data"); // todo better error messsage because thats not realy what we're testing
+                assert_msg(srb_reg_temp == rx_fifo_stim[rx_empty_fifo_ind], "Data in SPART RX queue matches transmitted data from TB");
                 srb.spart_reg_read(ADDR_SREG, srb_reg_temp);
-                assert_msg(srb_reg_temp[3:0] == (QUEUE_SIZE>>1)-rx_empty_fifo_ind-1, "Status register (RX) tracks after reading RX fifo");
+                assert_msg(srb_reg_temp[3:0] == (QUEUE_SIZE>>1)-rx_empty_fifo_ind-1, "Status register (RX) tracks after reading from RX queue");
             end
         end
     join
@@ -255,19 +269,12 @@ initial begin;
     // Now we empty the RX FIFO and make sure the data is correct
     for (rx_empty_fifo_ind = (QUEUE_SIZE >> 1); rx_empty_fifo_ind < STIM_QUEUE_SIZE; rx_empty_fifo_ind++) begin
         srb.spart_reg_read(ADDR_DBUF, srb_reg_temp);
-        assert_msg(srb_reg_temp == rx_fifo_stim[rx_empty_fifo_ind], "Received RX data matches queue data"); // todo better error messsage because thats not realy what we're testing
+        assert_msg(srb_reg_temp == rx_fifo_stim[rx_empty_fifo_ind], "Data in SPART RX queue matches transmitted data from TB");
         srb.spart_reg_read(ADDR_SREG, srb_reg_temp);
-        assert_msg(srb_reg_temp[3:0] == (STIM_QUEUE_SIZE)-rx_empty_fifo_ind-1, "Status register (RX) tracks after reading RX fifo");
+        assert_msg(srb_reg_temp[3:0] == (STIM_QUEUE_SIZE)-rx_empty_fifo_ind-1, "Status register (RX) tracks after reading from RX queue");
     end
 
-    // The RX queue should be full. we will from it and check it.
-    // for (both_fifo_ind = 0; both_fifo_ind < QUEUE_SIZE; both_fifo_ind++) begin
-    //     srb.spart_reg_write(ADDR_DBUF, tx_fifo_stim[both_fifo_ind]);
-    //     srb.spart_reg_read(ADDR_SREG, srb_reg_temp);
-    //     assert(srb_reg_temp[7:4] == QUEUE_SIZE-both_fifo_ind-1);
-    // end
-    // assert(tx_q_full);
-    
+    // end tests
 
     $display("\nYahoo!!! All Tests Passed\n");
     $finish();
@@ -276,12 +283,12 @@ end
 // This essentially acts as our simulated version of a UART
 // Wait for RX to go low (start bit)
 always @(negedge spart_tx) begin
-    $display("Starting a receive at %t..", $time);
-    recv_uart_rx(clk, spart_tx, baud_temp, uart_rx_temp);
+    $display("INFO: Starting a UART receive at %t..", $time);
+    recv_uart_rx(clk, spart_tx, curr_baud, uart_rx_temp);
     new_uart_rx_data = 1;
 end
 
-
+// 50MHz clock
 always
     #10 clk = ~clk;
 
